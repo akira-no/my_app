3848a9df43ce88feb446904cccb4ea36e9241924
akira-no <zzzzzgm4021@gmail.com>
2022-07-08 12:47:07 +0900

    view修正前の仮save

--- app/javascript/checkbox.js
+++ app/javascript/checkbox.js
@@ -1,6 +1,36 @@
-function formSwitch() {
-  categorySearch = document.getElementsByName("maker")
-    if (categorySearch[0].checked) {
-      document.getElementById("tagList").style.d = "";
-    } 
-}
+window.addEventListener("load", function() {
+
+
+//  formSwitch() {
+  const categorySearch = document.getElementsByName("maker'+ input'");
+
+  categorySearch.addEventListener('click', function() {
+    console.log("click ok")
+
+  });
+
+
+
+
+  // categorySearch = document.getElementById("tag");
+  // console.log("ok")
+  // categorySearch = document.getElementsByName("maker")
+    // if (categorySearch[0].checked) {
+      // document.getElementById("tagList").style.display = "block";
+    // } 
+    // // else {
+    //   // document.getElementById("tagList").style.display = "none";
+    // }
+});
+
+
+/* <script type="text/javascript"> */
+// function checkdiv( obj,id ) {
+// if( obj.checked ){
+// document.getElementById(check).style.display = "block";
+// }
+// else {
+// document.getElementById(check).style.display = "none";
+// }
+// }
+// </script>
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {
+    sign_up: ''
+  }
   root "plans#index"
   resources :plans, only: [:index, :new, :create] do
     collection do
