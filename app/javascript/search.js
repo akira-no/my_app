c4a08c5fde114ff1a8a70e33acec4de1575689b5
akira-no <zzzzzgm4021@gmail.com>
2022-07-14 12:25:42 +0900

    微修正

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -19,41 +19,7 @@ window.addEventListener("load", function() {
             `<li class = "search">${plan.item}</li>`
           )
         });
-
       })
-      // console.log(data);
-
     });
   });
 });
-
-
-
-
-
-
-
-
-// function search(){
-  
-  // const submit = document.getElementById("submit");
-  // const searchRender = document.getElementById("search-render")
-  //   submit.addEventListener("click", (e) => {
-  //     e.preventDefault();
-  //     const form = document.getElementById("detailed-search-form");
-  //     const formData = new FormData(form);
-  //     const XHR = new XMLHttpRequest();
-  //     XHR.open("POST", "/plans", true);
-  //     XHR.responseType = "json";
-  //     XHR.send(formData);
-  //     XHR.onload = () => {
-  //       searchRender.setAttribute("style", "display: block;")
-     
-  //       console.log(XHR.response);
-
-        
-  //     }
-      
-  //   });
-//   }
-// window.addEventListener("load", search);
